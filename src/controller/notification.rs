8aae46fd73b255e3e7610ea9ffc7d9927f6a5ef8
isaui <isadestroyed17@gmail.com>
2024-04-13 01:12:12 +0700

    Implement subscribe function in Notification service.

---
+++ src/controller/notification.rs
@@ -0,0 +1,15 @@
+use rocket::response::status::Created;
+use rocket::serde::json::Json;
+
+use bambangshop::Result;
+use crate::model::subscriber::Subscriber;
+use crate::repository::subscriber::SubscriberRepository;
+use  crate::service::notification::NotificationService;
+
+pub fn subscribe(product_type: &str, subscriber: Subscriber) -> Result<Subscriber>{
+    let product_type_upper: String = product_type.to_uppercase();
+    let product_type_str: &str = product_type_upper.as_str();
+    let subscriber_result: Subscriber =
+        SubscriberRepository::add(product_type_str, subscriber);
+    return Ok(subscriber_result);
+}
