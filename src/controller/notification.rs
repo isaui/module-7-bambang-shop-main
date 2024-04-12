11384428dd0d88d89c375d0105e5e1824507b20c
isaui <isadestroyed17@gmail.com>
2024-04-13 01:37:55 +0700

    Implement subscribe function in Notification service.

--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -5,7 +5,9 @@ use bambangshop::Result;
 use crate::model::subscriber::Subscriber;
 use crate::repository::subscriber::SubscriberRepository;
 use  crate::service::notification::NotificationService;
+use crate::service::product::ProductService;
 
+#[post("/subscribe/<product_type>", data = "<subscriber>")]
 pub fn subscribe(product_type: &str, subscriber: Subscriber) -> Result<Subscriber>{
     let product_type_upper: String = product_type.to_uppercase();
     let product_type_str: &str = product_type_upper.as_str();
@@ -13,3 +15,4 @@ pub fn subscribe(product_type: &str, subscriber: Subscriber) -> Result<Subscribe
         SubscriberRepository::add(product_type_str, subscriber);
     return Ok(subscriber_result);
 }
+
